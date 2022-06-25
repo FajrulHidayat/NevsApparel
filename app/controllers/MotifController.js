48804f8a9557a0e901fe3d02644728768bbb25e7
Fajrul Hidayat <fajrulknight2@gmail.com>
2022-06-25 20:54:32 +0800

    fix litle problem

--- app/controllers/MotifController.js
+++ app/controllers/MotifController.js
@@ -1,4 +1,4 @@
-const {Model} = require("../models")
+const {Motif} = require("../models")
 const multer = require("multer");
 const upload = multer().single("foto");
 const axios = require("axios")
@@ -18,9 +18,26 @@ class MotifController {
               }
 
             try{
-                console.log(req.file);
-                return res.status(200).json({ success: true,data:" error: error "});
+                if (!validation.success) return res.status(400).json(validation);
 
+      const imagePath = path.join(__dirname, "../../public/image");
+      const fileUpload = new resize(imagePath);
+      let foto = await fileUpload.save(req.file.buffer, req.file.originalname);
+      
+      axios.post(`https://api.imgbb.com/1/upload?key=4c40d02b3fab3d334e1c22deb47fbd91&image=https://nevsbe.herokuapp.com/image/${foto}`)
+      .then(async function (response) {
+        console.log(response);
+        imgbbResp=response.data.data.display_url
+        const motif = new Motif({
+          idDesign: req.body.idDesign,
+          urlDesign: imgbbResp,
+          
+        });
+        console.log(imgbbResp);
+        console.log(motif);
+        const dtMotif = await motif.save();
+        return res.status(200).json({ success: true, dtMotif });
+    })
             }catch(error){
                 return res.status(505).json({ success: false, error: error });
             }
