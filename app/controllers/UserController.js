f7adfa202bda9dae90c926f75c6c216f76ddf8ca
Fajrul Hidayat <fajrulknight2@gmail.com>
2022-08-02 19:53:47 +0800

    add id in response login

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,58 +1,58 @@
-const {User} = require("../models")
+const { User } = require("../models");
 
 class UserController {
-    async Registration(req,res){
-      let status;
-      let message;
-      let dtUser;
-        const user = new User({
-            name:req.body.name,
-            email:req.body.email,
-            password:req.body.password,
-            phone:req.body.phone,
-            level:req.body.level,
-        })
-        dtUser = await user.save()
-        if (dtUser) {
-          status=200
-          message="Registration Success"
-        } else {
-          status=401
-          message="Registration Failed"
-        }
-        return res.status(200).json({ success: true, data:dtUser });
+  async Registration(req, res) {
+    let status;
+    let message;
+    let dtUser;
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      phone: req.body.phone,
+      level: req.body.level,
+    });
+    dtUser = await user.save();
+    if (dtUser) {
+      status = 200;
+      message = "Registration Success";
+    } else {
+      status = 401;
+      message = "Registration Failed";
     }
-    async Login(req,res){
-        let dtUser
-        let status
-        let message
-        let response
-        const user = {
-            email:req.body.email,
-            password:req.body.password
-        }
-        dtUser = await User.findOne({ email: user.email }).exec()
-        if (!dtUser) {
-            status = 404;
-            message = "User not found";
-          } else {
-            if (user.password != dtUser.password) {
-              status = 401;
-              message = "Wrong Password";
-            } else {
-              
-              status = 200;
-              message = "Login Success";
-              response = {
-                name: dtUser.name,
-                email: dtUser.email,
-                level: dtUser.level,
-              };
-            }
-          }
+    return res.status(200).json({ success: true, data: dtUser });
+  }
+  async Login(req, res) {
+    let dtUser;
+    let status;
+    let message;
+    let response;
+    const user = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+    dtUser = await User.findOne({ email: user.email }).exec();
+    if (!dtUser) {
+      status = 404;
+      message = "User not found";
+    } else {
+      if (user.password != dtUser.password) {
+        status = 401;
+        message = "Wrong Password";
+      } else {
+        status = 200;
+        message = "Login Success";
+        response = {
+          id: dtUser._id,
+          name: dtUser.name,
+          email: dtUser.email,
+          level: dtUser.level,
+        };
+      }
+    }
 
-        return res.status(status).json({ message: message, data:response });
-    }
+    return res.status(status).json({ message: message, data: response });
+  }
 }
-const userController = new UserController()
-module.exports = userController
+const userController = new UserController();
+module.exports = userController;
