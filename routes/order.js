4bb36a0eca03186746cf5f7760f58ead80c187bc
Fajrul Hidayat <fajrulknight2@gmail.com>
2022-08-02 15:46:18 +0800

    add get all order

--- routes/order.js
+++ routes/order.js
@@ -3,5 +3,6 @@ const { order } = require("../app/controllers");
 
 const router = express.Router();
 router.post("/", order.Order);
+router.get("/AllOrder", order.getAllOrder);
 
 module.exports = router;
